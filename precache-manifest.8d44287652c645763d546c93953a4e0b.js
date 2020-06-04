f74f34a1512903f6d7e375cbf8cccde8e4442fa8
Hoa Nguyen <nhoa@tma.com.vn>
2020-06-04 12:55:39 +0700

    Updates

---
+++ precache-manifest.8d44287652c645763d546c93953a4e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1813cf14d74a9532407608672c853bb4",
+    "url": "/simple-react-weather-forecast/index.html"
+  },
+  {
+    "revision": "2b52ff485fdffc383b48",
+    "url": "/simple-react-weather-forecast/static/css/main.397ec257.chunk.css"
+  },
+  {
+    "revision": "cad026fa62968f11d558",
+    "url": "/simple-react-weather-forecast/static/js/2.66e1e214.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simple-react-weather-forecast/static/js/2.66e1e214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b52ff485fdffc383b48",
+    "url": "/simple-react-weather-forecast/static/js/main.c0b61d27.chunk.js"
+  },
+  {
+    "revision": "f77f3de518661d77ab36",
+    "url": "/simple-react-weather-forecast/static/js/runtime-main.7a877ee9.js"
+  },
+  {
+    "revision": "077965db38c370859d7d34287c7b41b5",
+    "url": "/simple-react-weather-forecast/static/media/cloud.077965db.svg"
+  }
+]);
