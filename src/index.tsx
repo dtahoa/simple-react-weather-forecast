f17c0ac6e06a4bb256e0f0d90ac4520064b8ad50
Hoa Nguyen <nhoa@tma.com.vn>
2020-06-10 18:06:53 +0700

    Adding redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import Logger from 'redux-logger';
+import { StoreContext } from 'redux-react-hook';
 
 import './index.css';
 import AppLayout from './layouts/app';
 import PageNotFound from './components/notfound';
 
 import * as serviceWorker from './serviceWorker';
+import apiReducer from './reducers/apiReducer';
+
+const reducers = combineReducers({ api: apiReducer });
+const middleWares = [thunk, Logger, promiseMiddleware];
+const createStoreWithMiddleWare = applyMiddleware(...middleWares)(createStore);
+const store = createStoreWithMiddleWare(reducers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={AppLayout} />
-        <Route component={PageNotFound} />
-      </Switch>
-    </BrowserRouter>
+    <StoreContext.Provider value={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={AppLayout} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </BrowserRouter>
+    </StoreContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
